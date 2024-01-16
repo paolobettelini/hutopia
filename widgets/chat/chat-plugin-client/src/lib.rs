da440c19bd2837175dc9f70bd59408c53830af74
paolobettelini <paolo.bettelini@samtrevano.ch>
2024-01-16 07:59:36 +0100

    minor fixes

--- widgets/chat/chat-plugin-client/src/lib.rs
+++ widgets/chat/chat-plugin-client/src/lib.rs
@@ -13,10 +13,11 @@ use uuid::Uuid;
 use chat_plugin_protocol::protocol::{Parcel, Settings};
 use protocol::message::*;
 
+// https://rustwasm.github.io/wasm-bindgen/examples/websockets.html
 
 const CUSTOM_HTML_TAG: &str = "widget-chat";
 
-// The boring part: a basic DOM component
+// The DOM component
 struct ChatComponent {
     ws: Rc<WebSocket>,
 }
@@ -122,26 +123,19 @@ impl CustomElement for ChatComponent {
         _name: String,
         _old_value: Option<String>,
         _new_value: Option<String>,
-    ) {
-        log("attribute changed");
-    }
+    ) { }
 
-    fn connected_callback(&mut self, _this: &HtmlElement) {
-        log("connected");
-    }
+    fn connected_callback(&mut self, _this: &HtmlElement) { }
 
-    fn disconnected_callback(&mut self, _this: &HtmlElement) {
-        log("disconnected");
-    }
+    fn disconnected_callback(&mut self, _this: &HtmlElement) { }
 
-    fn adopted_callback(&mut self, _this: &HtmlElement) {
-        log("adopted");
-    }
+    fn adopted_callback(&mut self, _this: &HtmlElement) { }
 }
 
-// wasm_bindgen entry point defines the Custom Element, then creates a few of them
+// wasm_bindgen entry point defines the custom element
 #[wasm_bindgen]
 pub fn run() -> Result<(), JsValue> {
+    console_log!("Running run");
     // define the Custom Element
     ChatComponent::define(CUSTOM_HTML_TAG);
 
@@ -150,49 +144,9 @@ pub fn run() -> Result<(), JsValue> {
 
 fn init_socket() -> WebSocket {
     let ws = WebSocket::new("ws://localhost:8080/widget_ws/chat").unwrap();
-    // TODO: you should switch to blob type for big transfers like files
     ws.set_binary_type(web_sys::BinaryType::Arraybuffer);
-    let _cloned_ws = ws.clone();
-    /*let onmessage_callback = Closure::<dyn FnMut(_)>::new(move |e: MessageEvent| {
-        // Handle difference Text/Binary,...
-        if let Ok(abuf) = e.data().dyn_into::<js_sys::ArrayBuffer>() {
-            console_log!("message event, received arraybuffer: {:?}", abuf);
-            let array = js_sys::Uint8Array::new(&abuf);
-            let len = array.byte_length() as usize;
-            console_log!("Arraybuffer received {}bytes: {:?}", len, array.to_vec());
-            // here you can for example use Serde Deserialize decode the message
-            // for demo purposes we switch back to Blob-type and send off another binary message
-            cloned_ws.set_binary_type(web_sys::BinaryType::Blob);
-            match cloned_ws.send_with_u8_array(&[5, 6, 7, 8]) {
-                Ok(_) => console_log!("binary message successfully sent"),
-                Err(err) => console_log!("error sending message: {:?}", err),
-            }
-        } else if let Ok(blob) = e.data().dyn_into::<web_sys::Blob>() {
-            console_log!("message event, received blob: {:?}", blob);
-            // better alternative to juggling with FileReader is to use https://crates.io/crates/gloo-file
-            let fr = web_sys::FileReader::new().unwrap();
-            let fr_c = fr.clone();
-            // create onLoadEnd callback
-            let onloadend_cb = Closure::<dyn FnMut(_)>::new(move |_e: web_sys::ProgressEvent| {
-                let array = js_sys::Uint8Array::new(&fr_c.result().unwrap());
-                let len = array.byte_length() as usize;
-                console_log!("Blob received {}bytes: {:?}", len, array.to_vec());
-                // here you can for example use the received image/png data
-            });
-            fr.set_onloadend(Some(onloadend_cb.as_ref().unchecked_ref()));
-            fr.read_as_array_buffer(&blob).expect("blob not readable");
-            onloadend_cb.forget();
-        } else if let Ok(txt) = e.data().dyn_into::<js_sys::JsString>() {
-            console_log!("message event, received Text: {:?}", txt);
-        } else {
-            console_log!("message event, received Unknown: {:?}", e.data());
-        }
-    });
-    // set message event handler on WebSocket
-    ws.set_onmessage(Some(onmessage_callback.as_ref().unchecked_ref()));
-    // forget the callback to keep it alive
-    onmessage_callback.forget();*/
 
+    let _cloned_ws = ws.clone();
     let onerror_callback = Closure::<dyn FnMut(_)>::new(move |e: ErrorEvent| {
         console_log!("error event: {:?}", e);
     });
@@ -203,7 +157,7 @@ fn init_socket() -> WebSocket {
     let onopen_callback = Closure::<dyn FnMut()>::new(move || {
         console_log!("socket opened");
 
-        // TOOD: Connect packet
+        // Connect packet
         let uuid = Uuid::new_v4();
         console_log!("Generating UUID: {uuid}");
 
